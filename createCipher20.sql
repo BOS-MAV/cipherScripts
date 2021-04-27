a849667a55b8e90a33354afaad088d7bb36928ad
Russo <John.Russo2@va.gov>
2021-04-27 11:26:53 -0400

    first set of tables

--- createCipher20.sql
+++ createCipher20.sql
@@ -27,4 +27,71 @@ create table POC.PhenoDataSourceLookup (
     constraint pdspk primary key (DataSourceID,PhenoTypeID)
 );
 
+create table POC.CUI (
+    CUIID           int primary key,
+    CUI             varchar(8),
+    CUIDesc         nvarchar(max)
+);
+
+create table POC.PhenoCUILookup (
+    PhenotypeID     int references POC.PhenoDef(PhenotypeID),
+    CUIID           int references POC.CUI(CUIID),
+    constraint pkclpk primary key (PhenotypeID,CUIID)
+);
+
+create table POC.ICDCode (
+    ICDCodeID       int identity primary key,
+    ICDCODE         varchar(8),
+    ICDType         nvarchar(max),
+    ICDDesc         nvarchar(max)
+);
+
+create table POC.PhenoICDCodeLookup (
+    ICDCodeID       int references POC.ICDCode(ICDCodeID),
+    PhenotypeID     int references POC.PhenoDef(PhenotypeID),
+    constraint phICDpk primary key (ICDCodeID,PhenotypeID)
+);
+
+create table POC.Property (
+    PropertyID      int primary key,
+    PropertyName    varchar(50),
+    PropertyType    varchar(10),
+    PropertyCode    nvarchar(max)
+);
+
+create table POC.PhenotypeProperties (
+    PhenotypeID     int references POC.PhenoDef(PhenotypeID),
+    PropertyID      int references POC.Property(PropertyID),
+    constraint phproppk primary key (PhenotypeID,PropertyID)
+);
+
+create table POC.AltName (
+    AltNameID       int identity primary key,
+    AltName         varchar(50)
+);
+
+create table POC.PhenoAltNameLookup (
+    AltNameID       int references POC.AltName(AltNameID),
+    PhenoTypeID     int references POC.PhenoDef(PhenoTypeID),
+    constraint phanpk primary key (AltNameID,PhenoTypeID)
+);
+
+create table POC.CodeBlock (
+    CodeBlockID     int identity primary key,
+    CodeBlockLang   varchar(50),
+    CodeBlockDesc   nvarchar(max),
+    CodeBlockText   nvarchar(max),
+    PhenotypeID     int references POC.PhenoDef(PhenotypeID)
+);
+
+create table POC.Lifestyle_Envi (
+    LsEnvPhenoTypeID            int references POC.PhenoDef(PhenotypeID),
+    LEType                      varchar(50),
+    AlgorithmValidation         nvarchar(max),
+    ValidationDesc              nvarchar(max),
+    AlgoPerformanceMeasures     nvarchar(max),
+    constraint lfspk primary key (LsEnvPhenoTypeID)
+);
+
+
 
