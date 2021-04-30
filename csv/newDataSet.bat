0e8dbd5f76f4d8a2ec63bbdbbf361e1189bd5050
BOS-MAV <52087638+BOS-MAV@users.noreply.github.com>
2021-04-30 02:00:54 -0400

    Minor tweaks

---
+++ csv/newDataSet.bat
@@ -0,0 +1,50 @@
+curl "https://api.mockaroo.com/api/1de95590?count=1000&key=faff7a60" > "Population.csv"
+curl https://api.mockaroo.com/api/datasets/Population -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @population.csv
+curl "https://api.mockaroo.com/api/9332e520?count=1000&key=faff7a60" > "PhenoDef.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoDef -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoDef.csv
+curl "https://api.mockaroo.com/api/9f1bf480?count=1000&key=faff7a60" > "DataSource.csv"
+curl https://api.mockaroo.com/api/datasets/DataSource -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @DataSource.csv
+curl "https://api.mockaroo.com/api/db82b290?count=1000&key=faff7a60" > "PhenoDataSourceLookup.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoDataSourceLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoDataSourceLookup.csv
+curl "https://api.mockaroo.com/api/e3dc37d0?count=1000&key=faff7a60" > "CUI.csv"
+curl https://api.mockaroo.com/api/datasets/CUI -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @CUI.csv
+curl "https://api.mockaroo.com/api/2b7eb1d0?count=1000&key=faff7a60" > "PhenoCUILookup.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoCUILookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoCUILookup.csv
+curl "https://api.mockaroo.com/api/075b4170?count=1000&key=faff7a60" > "ICDCode.csv"
+curl https://api.mockaroo.com/api/datasets/ICDCode -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @ICDCode.csv
+curl "https://api.mockaroo.com/api/2446f4c0?count=1000&key=faff7a60" > "PhenoICDCodeLookup.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoICDCodeLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoICDCodeLookup.csv
+curl "https://api.mockaroo.com/api/14aa62a0?count=1000&key=faff7a60" > "Property.csv"
+curl https://api.mockaroo.ceom/api/datasets/Property -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @Property.csv
+curl "https://api.mockaroo.com/api/864730c0?count=1000&key=faff7a60" > "PhenotypeProperties.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoTypeProperties -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoTypeProperties.csv
+curl "https://api.mockaroo.com/api/d837f800?count=1000&key=faff7a60" > "AltName.csv"
+curl https://api.mockaroo.com/api/datasets/AltName -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @AltName.csv
+curl "https://api.mockaroo.com/api/ce56a760?count=1000&key=faff7a60" > "PhenoAltNameLookup.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoAltNameLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoAltNameLookup.csv
+curl "https://api.mockaroo.com/api/49b10310?count=1000&key=faff7a60" > "CodeBlock.csv"
+curl https://api.mockaroo.com/api/datasets/CodeBlock -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @CodeBlock.csv
+curl "https://api.mockaroo.com/api/32afca60?count=1000&key=faff7a60" > "Disease.csv"
+curl https://api.mockaroo.com/api/datasets/Disease -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @Disease.csv
+curl "https://api.mockaroo.com/api/084f8280?count=1000&key=faff7a60" > "DiseaseDomain.csv"
+curl https://api.mockaroo.com/api/datasets/DiseaseDomain -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @DiseaseDomain.csv
+curl "https://api.mockaroo.com/api/df7207a0?count=1000&key=faff7a60" > "DiseaseDiseaseDomainLookup.csv"
+curl https://api.mockaroo.com/api/datasets/DiseaseDiseaseDomainLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @DiseaseDiseaseDomainLookup.csv
+curl "https://api.mockaroo.com/api/ab992d60?count=1000&key=faff7a60" > "Lab.csv"
+curl https://api.mockaroo.com/api/datasets/Lab -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @Lab.csv
+curl "https://api.mockaroo.com/api/33db69d0?count=1000&key=faff7a60" > "LOINC.csv"
+curl https://api.mockaroo.com/api/datasets/LOINC -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @LOINC.csv
+curl "https://api.mockaroo.com/api/718df2d0?count=1000&key=faff7a60" > "LabLOINCLookup.csv"
+curl https://api.mockaroo.com/api/datasets/LabLOINCLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @LabLOINCLookup.csv
+curl "https://api.mockaroo.com/api/09700600?count=1000&key=faff7a60" > "Author.csv"
+curl https://api.mockaroo.com/api/datasets/Author -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @Author.csv
+curl "https://api.mockaroo.com/api/a8c12550?count=1000&key=faff7a60" > "Publication.csv"
+curl https://api.mockaroo.com/api/datasets/Publication -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @Publication.csv
+curl "https://api.mockaroo.com/api/2a46fe20?count=1000&key=faff7a60" > "PublicationAuthorLookup.csv"
+curl https://api.mockaroo.com/api/datasets/PublicationAuthorLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PublicationAuthorLookup.csv
+curl "https://api.mockaroo.com/api/73b78f00?count=1000&key=faff7a60" > "PhenoPublicationLookup.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoPublicationLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoPublicationLookup.csv
+curl "https://api.mockaroo.com/api/70cf38d0?count=1000&key=faff7a60" > "PhenoAuthorLookup.csv"
+curl https://api.mockaroo.com/api/datasets/PhenoAuthorLookup -X POST  -H "X-Api-KEY: faff7a60" -H "Content-Type: text/csv" --data-binary @PhenoAuthorLookup.csv
+
+
